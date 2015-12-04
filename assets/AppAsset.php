271f4977786b19a57f0e6d494c84884fcac37eda
Ludovit Scholtz <ludko2@gmail.com>
2015-12-04 13:55:28 +0100

    db obci do stranky, pridany select2

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -16,9 +16,11 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css',
     ];
     public $js = [
         'js/site.js',
+        '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',

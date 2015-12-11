1aa48f8f2a57e167470dab4f60453f8ecd542d99
Ludovit Scholtz <ludko2@gmail.com>
2015-12-11 01:27:37 +0100

    sirka vyberu adresy na 100%

--- js/site.js
+++ js/site.js
@@ -155,7 +155,7 @@ function nastavObec() {
 }
 $(function () {
   nastavObec();
-  $("#addressslovakia-city").select2();
+  $("#addressslovakia-city").select2({width:'100%'});
 });
 
 function createDocument(preview) {

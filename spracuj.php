54e8a775f7f616ae78a0061102c7f264770b2dd0
roman3x <roman3x@users.noreply.github.com>
2015-12-14 05:04:01 +0100

    Nepouzivanie nadbytocnych emailov
    #47 Podla zoznamu a pravidiel sa budun/nebudu posielat a preferovat emaily. Ak nie je dost preferencnych, poslu sa vsetky

--- spracuj.php
+++ spracuj.php
@@ -2,6 +2,11 @@
 require_once('c:\webserver\vhost\AsyncWeb\gitAW\AsyncWeb\src\AsyncWeb\Text\Texts.php');
 use AsyncWeb\Text\Texts;
 var_dump(Texts::clear("NRIčouťľéíáťší+"));
+
+$aForcedEmails = array('starostka@karlovaves.sk','radnica@mestosnv.sk','kravany@kravany.com','');//bude priradeny iba jediny pre obec
+$aDisabledEmails = array('daniel.juracek@bosaca.eu','obec.bartosovce@wi-net.sk','oubenice@gaya.sk','betlanovce@stonline.sk','oub.kostol@apo.sk','dusan.zeliznak@gmail.com','obecboliarov@netkosice.sk','oubajtava@mail.t-com.sk','babindol@babindol.sk','peter.nemecek@obecbab.sk');
+$aPreferableEmailParts = array( 'podatelna','obec', 'ocu', 'ou', 'obu', 'urad', 'mu','msu','mesto','sekretariat','kancelaria','obecnyurad','miestnyurad','mestskyyurad','info','referent','NAZOVOBCE@NAZOVOBCE.SK','@NAZOVOBCE.SK','NAZOVOBCE','NAZOVOBCE@','primator','primatorka','starosta','starostka','kancelariaprimatora','prednosta','prednostka'); 
+
 $i = 0;
 //$out["Mimo SR"]["Mimo SR"]["mvsr"] = "['Ministerstvo vnútra Slovenskej republiky','odbor volieb, referenda a politických strán','Drieňová','22','826 86','Bratislava 29','volby@minv.sk','','','','Ministerstvo vnútra Slovenskej republiky']";
 $out = array();
@@ -17,147 +22,11 @@ if (($handle = fopen("../obce_13_12_2015 V2.txt", "r")) !== FALSE) {
 						$n2k[$v] = $k;	
 					}
 				}else{
-					$email = $data[$n2k["email"]];
-					$email = str_replace(",",";",$email);
-					$emaily = explode(";",$email);
-					
-					if(count($emaily) != 1){
-						foreach($emaily as $k=>$em){
-							if(!$em){unset($emaily[$k]);}
-							$em = trim($em);
-							if(strpos($em,"podatelna") !== false){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"sekretariat") !== false){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"obecnyurad") !== false){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"ocu") === 0){
-								
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"ou") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"obec.") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"miestnyurad") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"urad@") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"mesto@") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"msu.") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"obu@") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"msu@") === 0){
-								$emaily = array($em);break;
-							}
-							if(strpos($em,"info@") !== false){
-								$emaily = array($em);break;
-							}
-							
-							if($em == "starostka@karlovaves.sk") continue;
-							
-							if(strpos($em,"starosta") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"hovorca") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"starostka") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"matrika") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"primator") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"primatorka") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"kancelariaprimatora") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"primator") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"prednosta") !== false){
-								unset($emaily[$k]);
-							}
-							if(strpos($em,"prednostka") !== false){
-								unset($emaily[$k]);
-							}
-							if($em == "peter.nemecek@obecbab.sk"){
-								unset($emaily[$k]);
-							}
-							if($em == "babindol@babindol.sk"){ // alt obec@babindol.sk
-								unset($emaily[$k]);
-							}
-							if($em == "obec@babina.sk" || $em =="spravca@babina.sk" || $em=="ekonom@babina.sk"){ // alt podatelna@babina.sk
-								unset($emaily[$k]);
-							}
-							if($em == "babinec2010@gmail.com"){ // alt ocu@babinec.info"
-								unset($emaily[$k]);
-							}
-							if($em == "oubajtava@mail.t-com.sk"){ // alt oubajtava@stonline.sk
-								unset($emaily[$k]);
-							}
-							if($em == "nadezda.babiakova@banskastiavnica.sk" || $em=="ivana.ondrejmiskova@banskastiavnica.sk"){ // alt msu@banskastiavnica.sk
-								unset($emaily[$k]);
-							}
-							if($em == "obec.bartosovce@wi-net.sk"){ // alt obec.bartosovce@isomi.sk
-								unset($emaily[$k]);
-							}
-							if($em == "oubenice@gaya.sk"){ // alt benice@benice.sk
-								unset($emaily[$k]);
-							}
-							if($em == "betlanovce@stonline.sk"){ // alt obecbetlanovce@stonline.sk
-								unset($emaily[$k]);
-							}
-							if($em == "oub.kostol@apo.sk"){ // alt oub.kostol@stonline.sk
-								unset($emaily[$k]);
-							}
-							if($em == "oub.kostol@apo.sk"){ // alt oub.kostol@stonline.sk
-								unset($emaily[$k]);
-							}
-							if($em == "terezia.foldvaryova@blatnanaostrove.sk"){ // obec@blatnanaostrove.sk
-								unset($emaily[$k]);
-							}
-							if($em == "laco@svslm.sk"){ // alt obec@bobrovcek.sk
-								unset($emaily[$k]);
-							}
-							if($em == "dusan.zeliznak@gmail.com"){ // alt ZeliP@zoznam.sk
-								unset($emaily[$k]);
-							}
-							if($em == "obecboliarov@netkosice.sk"){ // alt obec@boliarov.sk
-								unset($emaily[$k]);
-							}
-							if($em == "daniel.juracek@bosaca.eu"){ // alt "bosaca@bosaca.eu"
-								unset($emaily[$k]);
-							}
-							
-						}
-					}
-					if(count($emaily) != 1){
-						/*var_dump($i);
-						var_dump($emaily);
-						var_dump($data[$n2k["email"]]);
-						//exit;
-						/**/
-					}
-					$e = implode(";",$emaily);
 					$name = Texts::clear($data[$n2k["obec"]]);
+					
+					$e = get_relevant_emails( $data[$n2k["email"]], $name );
+					//if( $e ) echo $name,":\t",$e,'<br>';//for debug
+					
 					$out[$data[$n2k["kraj"]]][$data[$n2k["okres"]]][$name] = "['".$data[$n2k["urad"]]."','','".$data[$n2k["ulica"]]."','".$data[$n2k["cislo"]]."','".$data[$n2k["psc"]]."','".$data[$n2k["posta"]]."','".$e."','".$data[$n2k["predvolba"]]."','".$data[$n2k["telefon"]]."','".$data[$n2k["mobil"]]."','".$data[$n2k["obec"]]."']";
 					
 					$data[$n2k["pocetobyvatelov"]] = str_replace(" ","",$data[$n2k["pocetobyvatelov"]]);
@@ -259,28 +128,88 @@ function get_top_cities(){
 }
 
 
-//$aPreferableEmails = array( 'podatelna','info','sekretariat','obec','ocu','ou','starosta','starostka'); 
-function get_relevant_email($emails){
-	global $aPreferableEmails;
+function get_relevant_emails($sEmails, $sObecClearName){
+	global $aForcedEmails,$aDisabledEmails,$aPreferableEmailParts;
 	
+	$bestEmailsWithDomain = array();
 	$bestEmails = array();
-	
-	if(count($emails) != 1){
-		foreach( $emails as $email ){
-			$em = explode( '@', $email, 2);
-			
-			if( in_array($em[0], $aPreferableEmails) ){
-				$bestEmails[ array_search($em[0], $aPreferableEmails) ] = $email;
+
+	$sEmails = str_replace( array(",","\t",' '), array(";",'',''), $sEmails);
+	$aEmails = explode(";",$sEmails);
+	//echo "<bR>BEG:";var_dump($aEmails,$sObecClearName);
+	if(count($aEmails) > 1){
+		foreach($aForcedEmails as $sForcedEmail){
+			if( $pos = array_keys($aEmails, $sForcedEmail) ){
+				return $aEmails[$pos];
 			}
+		}
+
+		foreach( $aEmails as $k => $sEmail ){
+			if( $pos = array_search($sEmail, $aDisabledEmails) !== false ){
+				unset( $aEmails[$k]);continue;
+			}
+			
+			$sObecCClearName = str_replace('-','',$sObecClearName);
+			$sEmailForSearch = str_replace(array('.','-'),'',$sEmail);
+			
+			$em = explode( '@', trim($sEmailForSearch), 2);
+			
+			$emBeforeAt = $em[0];
+			$emAfterAt = $em[1];
+			
+			$t = false;
+			if( ($t = array_search($emBeforeAt, $aPreferableEmailParts) ) !== false ){
+			}elseif( $sEmail == $sObecCClearName.'@'.$sObecCClearName.'sk' ){
+				$bestEmailsWithDomain[ 'NAZOVOBCE@NAZOVOBCE.SK' ] = $sEmail	;
+				$t = array_search('NAZOVOBCE@NAZOVOBCE.SK', $aPreferableEmailParts);
+			}elseif( $emAfterAt == $sObecCClearName.'sk' ){
+				$bestEmailsWithDomain[ '@NAZOVOBCE.SK' ] = $sEmail;
+				$t = array_search('@NAZOVOBCE.SK', $aPreferableEmailParts);
+			}elseif( strpos($sEmail, $sObecCClearName.'@' ) === 0 ){
+				$bestEmails[ 'NAZOVOBCE@' ] = $sEmail;
+				$t = array_search('NAZOVOBCE@', $aPreferableEmailParts);
+			}elseif( $emBeforeAt == $sObecCClearName ){
+				$bestEmails[ 'NAZOVOBCE' ] = $sEmail;
+				$t = array_search('NAZOVOBCE', $aPreferableEmailParts);
+			}
+			//var_dump(__LINE__, $t,$sObecCClearName,$emBeforeAt,$emAfterAt, '======',$emAfterAt,$sObecCClearName.'sk','********');echo '<br>';
+			
+			
+			if( $t == false ){
+				foreach( $aPreferableEmailParts as $k => $aPreferableEmailPart ){
+					if( strpos($emBeforeAt, $aPreferableEmailPart ) !== false ){
+						$t = array_search($aPreferableEmailPart, $aPreferableEmailParts);
+					}
+				}
+			}
+			
+			if( $t !== false ){
+				if( $emAfterAt == $sObecCClearName.'.sk' or $emAfterAt == 'obec'.$sObecCClearName.'.sk' or $emAfterAt == $sObecCClearName.'obec.sk' or $emAfterAt == $sObecCClearName.'mesto.sk' or $emAfterAt == 'mesto'.$sObecCClearName.'.sk' ){
+					$bestEmailsWithDomain[ $t ] = $sEmail;
+				}else{
+					$bestEmails[ $t ] = $sEmail;
+				}
+			}
+		}
+		
+		if( $bestEmailsWithDomain ){
+			ksort($bestEmailsWithDomain);
+			//var_dump(__LINE__,$bestEmailsWithDomain);
+			return reset($bestEmailsWithDomain);
 		}
 		
 		if( $bestEmails ){
 			ksort($bestEmails);
-			
+			//var_dump(__LINE__,$bestEmails);
 			return reset($bestEmails);
 		}
+	}else{
+		return implode(";",$aEmails);
 	}
 	
-	return implode(";",$emails);
+	
+	$nasobneEmaily = implode(";",$aEmails);
+	//if( count( $aEmails ) > 1 ) echo $sObecClearName,":\t",$nasobneEmaily,'<br>';//debug
+	return $nasobneEmaily;
 }
 
